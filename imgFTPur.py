17875c1ace4bdc14da18110924475f29cd2ad7cd
foothillsgeek <derek.newland@gmail.com>
2015-06-12 21:37:53 -0400

    FTP functionality implemented.

--- imgFTPur.py
+++ imgFTPur.py
@@ -3,11 +3,15 @@
 import ConfigParser
 import sys
 
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
 def list_config(config):
     for section in config.sections():
         print "[" + section + "]"
         for option in config.options(section):
-            print option + ": " + config.get(section,option)
+            print option + ": " + config.get(section, option)
 
 def get_config(conf_file):
     config = ConfigParser.ConfigParser()
@@ -17,7 +21,20 @@ def get_config(conf_file):
     else:
         return config
 
+def ftpd(config):
+    authorizer = DummyAuthorizer()
+    authorizer.add_user(
+        str(config.get('FTPSettings', 'username')), 
+        config.get('FTPSettings', 'password'), 
+        config.get('FTPSettings', 'incomingfolder'), 
+        perm="elradfmw")
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    server = FTPServer((config.get('FTPSettings', 'ipaddress'), config.get('FTPSettings', 'port')), handler)
+    server.serve_forever()
+
 config_file = 'imgFTPur.conf'
 
 config = get_config(config_file)
-list_config(config)
+#list_config(config)
+ftpd(config)
