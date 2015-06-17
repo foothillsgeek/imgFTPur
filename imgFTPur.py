8165f7014beca187ec737e964b39ddd55274c416
foothillsgeek <derek.newland@gmail.com>
2015-06-16 21:55:37 -0400

    Added argparse argument parsing capabilities

--- imgFTPur.py
+++ imgFTPur.py
@@ -1,11 +1,17 @@
 #/usr/bin/python env
 
+import argparse
 import ConfigParser
 import sys
 
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--list_config', action="store_true")
+    return parser.parse_args()
 
 def list_config(config):
     for section in config.sections():
@@ -36,5 +42,10 @@ def ftpd(config):
 config_file = 'imgFTPur.conf'
 
 config = get_config(config_file)
-#list_config(config)
+
+args = arg_parser()
+if args.list_config:
+    list_config(config)
+    sys.exit(1)
+
 ftpd(config)
